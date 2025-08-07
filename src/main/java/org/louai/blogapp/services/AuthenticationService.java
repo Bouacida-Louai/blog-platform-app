c1b38d2247bf0468a62172767baeb6c48b5bb9a6
Bouacida-Louai <abderrahmane.bouacida@univ-constantine2.dz>
2025-08-07 19:24:33 +0100

    Part 02

---
+++ src/main/java/org/louai/blogapp/services/AuthenticationService.java
@@ -0,0 +1,9 @@
+package org.louai.blogapp.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface AuthenticationService {
+    UserDetails authenticate(String email, String password) ;
+    String generateToken(UserDetails userDetails);
+
+}
